f172745950be4c9ed89ba341456543f989550ddd
Catherine Bates <catherine.bates@stockport.gov.uk>
2023-02-20 22:16:29 +0000

    TODAY

--- today.js
+++ today.js
@@ -2,4 +2,5 @@
 console.log ("BENTS LANE SOCIAL INSTITUTE LOWER BENTS LANE" .toLocaleLowerCase());
 console.log ("ANDY POWELL'S BIG BAND SWING ON A SUNDAY AFTERNOON" .toLocaleLowerCase());
 console.log ("BLAH, BLAH, BLAH" .toLocaleLowerCase());
+console.log ("BENTS LANE SOCIAL INSTITUTE LOWER BENTS LANE" .toLocaleLowerCase());
 
