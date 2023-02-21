13d13f0abd14a1598e16a7212e8d20260907f1d2
Catherine Bates <catherine.bates@stockport.gov.uk>
2023-02-21 12:04:58 +0000

    GM

--- today.js
+++ today.js
@@ -3,4 +3,4 @@ console.log ("BENTS LANE SOCIAL INSTITUTE LOWER BENTS LANE" .toLocaleLowerCase()
 console.log ("ANDY POWELL'S BIG BAND SWING ON A SUNDAY AFTERNOON" .toLocaleLowerCase());
 console.log ("BLAH, BLAH, BLAH" .toLocaleLowerCase());
 console.log ("BENTS LANE SOCIAL INSTITUTE LOWER BENTS LANE" .toLocaleLowerCase());
-
+console.log ("GREAT MOOR LIBRARY GLADSTONE STREET, GREAT MOOR, STOCKPORT, STOCKPORT, SK2 7QF" .toLocaleLowerCase());
